d72c2a1e3e36fa774dfb64faa28cd1efabfabf61
Andrew Harris <me@drew-harris.com>
2015-07-06 22:49:22 -0400

    Added more functionality

--- set01/challenge01/HexToB64.h
+++ set01/challenge01/HexToB64.h
@@ -1,2 +1,4 @@
 int HexCharToInt(char hex);
 int HexStringToInt(char * hex);
+char IntToB64Char(int num);
+char * IntToB64String(int num)
--- set01/challenge01/test.c
+++ set01/challenge01/test.c
@@ -2,10 +2,8 @@
 #include <assert.h>
 #include "HexToB64.h"
 
-void TestHexCharToInt()
+void TestHexCharToInt(int * success, int * failure)
 {
-    printf("Testing HexCharToInt()\n");
-    
     int i;
     char test[22] =
     {
@@ -24,15 +22,21 @@ void TestHexCharToInt()
     for (i = 0; i < 22; i++)
     {
         printf("HexCharToInt(\"%c\")...", test[i]);
-        assert(HexCharToInt(test[i]) == expected[i]);
-        printf("PASS\n");
+        if (HexCharToInt(test[i]) == expected[i])
+        {
+            printf("PASS\n");
+            (*success)++;
+        }
+        else
+        {
+            printf("FAIL (%i != %i)\n", HexCharToInt(test[i]), expected[i]);
+            (*failure)++;
+        }
     }
 }
 
-void TestHexStringToInt()
-{
-    printf("Testing HexStringToInt()\n");
-    
+void TestHexStringToInt(int * success, int * failure)
+{    
     int i;
     char * hexVals[6] = {"492", "76d", "206b69", "1", "12", "1234"};
     int intVals[6] = {1170, 1901, 2124649, 1, 18, 4660};
@@ -41,14 +45,72 @@ void TestHexStringToInt()
     for (i = 0; i < 6; i++)
     {
         printf("HexStringToInt(\"%s\")...", hexVals[i]);
-        assert(HexStringToInt(hexVals[i]) == intVals[i]);
-        printf("PASS\n");
+        if (HexStringToInt(hexVals[i]) == intVals[i])
+        {
+            printf("PASS\n");
+            (*success)++;
+        }
+        else
+        {
+            printf("Fail (%i != %i)\n", HexStringToInt(hexVals[i]),
+                    intVals[i]);
+            (*failure)++;
+        }
+    }
+}
+
+void TestIntToB64Char(int * success, int * failure)
+{
+    int i;
+    int test[11] = {0, 15, 25, 26, 47, 51, 52, 55, 61, 62, 63};
+    char result[11] = { 'A', 'P', 'Z', 'a', 'v', 'z',
+                        '0', '3', '9', '+', '/'};
+    
+    for (i = 0; i < 11; i++)
+    {
+        printf("IntToB64Char(\"%i\")...", test[i]);
+        if (IntToB64Char(test[i]) == result[i])
+        {
+            printf("PASS\n");
+            (*success)++;
+        }
+        else
+        {
+            printf("FAIL (%c != %c)", IntToB64Char(test[i]), result[i]);
+            (*failure)++;
+        }
+    }
+}
+
+void TestIntToB64String(int * success, int * failure)
+{
+    int i;
+    int test[11] = {0, 15, 25, 26, 47, 51, 52, 55, 61, 62, 63};
+    char result[11] = { 'A', 'P', 'Z', 'a', 'v', 'z',
+                        '0', '3', '9', '+', '/'};
+    
+    for (i = 0; i < 11; i++)
+    {
+        printf("IntToB64String(\"%i\")...", test[i]);
+        if (IntToB64String(test[i]) == result[i])
+        {
+            printf("PASS\n");
+            (*success)++;
+        }
+        else
+        {
+            printf("FAIL (%s != %s)", IntToB64String(test[i]), result[i]);
+            (*failure)++;
+        }
     }
 }
 
 int main()
 {
-    TestHexCharToInt();
-    TestHexStringToInt();
-    printf("Tests completed\n");
+    int success = 0, failure = 0;
+    TestHexCharToInt(&success, &failure);
+    TestHexStringToInt(&success, &failure);
+    TestIntToB64Char(&success, &failure);
+    printf("Tests completed: %i successes and %i failures\n",
+            success, failure);
 }
